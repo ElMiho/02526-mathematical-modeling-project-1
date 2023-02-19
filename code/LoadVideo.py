ab6ee40f20c7a690ab51daa5c1d304d1bbce6398
jacobtuxen <jacob.tuxen@outlook.com>
2023-02-19 19:09:05 +0100

    added mp4

--- code/LoadVideo.py
+++ code/LoadVideo.py
@@ -1,11 +1,11 @@
 import cv2
 
 
-vidcap = cv2.VideoCapture('./vante.mp4')
+vidcap = cv2.VideoCapture('./Users/tuxen/Desktop/02526 Mathematical Modeling/optic flow/02526-mathematical-modeling-project-1/code/data/nySkygge.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("vanteImages/frame%d.png" % count, image)     # save frame as PNG file      
+  cv2.imwrite("/Users/tuxen/Desktop/02526 Mathematical Modeling/optic flow/02526-mathematical-modeling-project-1/code/nySkyggeImages/frame%d.png" % count, image)     # save frame as PNG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
